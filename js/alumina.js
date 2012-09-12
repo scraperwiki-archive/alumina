74abe1571880376e40334ccbde790ff80df7a051
Zarino Zappia <mail@zarino.co.uk>
2012-09-12 21:57:44 +0100

    added showdown.js to convert markdown to HTML

--- js/alumina.js
+++ js/alumina.js
@@ -4,9 +4,11 @@ $(function(){
         url: "README.symlink.md",
         dataType: 'text',
         cache: false,
-        success: function(data){
+        success: function(text){
             $('#notes .loading').remove();
-            $('#notes').append('<pre>' + data + '</pre>');
+            var converter = new Showdown.converter();
+            var html = converter.makeHtml(text);
+            $('#notes').append(html);
         }, error: function(jqXHR, textStatus, errorThrown){
             $('#notes .loading').remove();
             $('<div>').addClass('alert').html('<button type="button" class="close" data-dismiss="alert">×</button> <strong>Sorry!</strong> We couldn&rsquo;t find your project&rsquo;s README.md file. Are you sure it exists?').appendTo('#notes');
@@ -48,7 +50,7 @@ $(function(){
                     $('.tables .table').eq($(this).prevAll().length).show().siblings('.table').hide();
                 });
                 $('<a href="#">' + table['name'] + '</a>').appendTo($li);
-                var $table = $('<div>').addClass('table').css('height', 400);
+                var $table = $('<div>').addClass('table').css('height', 300);
                 $.ajax({
                     url: "../../sqlite",
                     data: {
@@ -80,29 +82,6 @@ $(function(){
             $('#datagrid').hide();
         }
     });
-
-    /*
-
-    var data = [
-        {id: 0, date: '2011-01-01', x: 1, y: 2, z: 3, country: 'DE', geo: {lat:52.56, lon:13.40} },
-        {id: 1, date: '2011-02-02', x: 2, y: 4, z: 24, country: 'UK', geo: {lat:54.97, lon:-1.60}},
-        {id: 2, date: '2011-03-03', x: 3, y: 6, z: 9, country: 'US', geo: {lat:40.00, lon:-75.5}},
-        {id: 3, date: '2011-04-04', x: 4, y: 8, z: 6, country: 'UK', geo: {lat:57.27, lon:-6.20}},
-        {id: 4, date: '2011-05-04', x: 5, y: 10, z: 15, country: 'UK', geo: {lat:51.58, lon:0}},
-        {id: 5, date: '2011-06-02', x: 6, y: 12, z: 18, country: 'DE', geo: {lat:51.04, lon:7.9}}
-    ];
-    var dataset = new recline.Model.Dataset({
-        records: data
-    });
-    var $el = $('#datagrid');
-    var grid = new recline.View.SlickGrid({
-        model: dataset,
-        el: $el
-    });
-    grid.visible = true;
-    grid.render();
-
-    */
 
     var $hds = $('<span>').addClass('drop-shadow').appendTo('.page-header');
     $(window).scroll(function(){
